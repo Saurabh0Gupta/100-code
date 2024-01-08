688bc00295c1ca83723349007c6dc6ecab75aebf
Saurabh0Gupta <saurabhgupta7814@gmail.com>
2024-01-08 23:55:02 +0530

    Code_20

---
+++ Code_21.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Code_21{
+    public static void main(String ag[]){
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        for(int i=1; i<=num; i++){
+            if(num%i==0){
+                if(i%)
+                System.out.print(i+",");
+            }
+        }
+    }
+}
