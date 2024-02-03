bc12f8df2ed1c79980a74c6666dccc7d42bea690
Saurabh0Gupta <saurabhgupta7814@gmail.com>
2024-02-03 23:54:39 +0530

    commit

--- Recursion/LargestNum.java
+++ Recursion/LargestNum.java
@@ -5,10 +5,15 @@ class LargestNum{
         int arr[]={12,34,64,235,689,654,32};
         int size=7;
         int result=largestNum(arr,size,0);
+
+        for(int i=0; i<size; i++){
+            System.out.print(arr[i]+" ");
+        }
     }
-    static int largestNum(int arr[],int size,int i){
-        if(i>)
-        if()
+    static int largestNum(int arr[],int n){
+        if(n==1)
+        return arr[0];
+        int maxInt=largestNum(arr,n-1);
     }
-    
+
 }
