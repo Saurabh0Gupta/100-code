118f7b394bf43d8c1960eb805b77902757590f51
Saurabh0Gupta <saurabhgupta7814@gmail.com>
2024-02-26 01:30:30 +0530

    commit

--- Code_19.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-class Code_19{
-    public static void main(String er[]){
-        Scanner sc=new Scanner(System.in);
-        int num=sc.nextInt();
-        int pow=sc.nextInt();
-        System.out.print("power of given number is "+(int)Math.pow(num,pow));
-    }
-}
